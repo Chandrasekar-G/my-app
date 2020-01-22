93f3ab66240ebb7af6bffd215635f14b22564ffc
Chandrasekar-G <chandrasekarg@outlook.com>
2020-01-22 21:51:26 +0530

    Component intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'my-app';
+//   str = "a";
+//   arr = [1, 2, 3 ];
+
+//   add() {
+//     return this.title;
+//   }
+
+
+// }
+
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Hi there!</h1>
+    <div>{{ title }}</div>
+  `,
+  styles: [
+    `
+    div {
+      color: red;
+    }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'my-app';
+
+  title = 'App title';
 }
